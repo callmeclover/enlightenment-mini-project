2f443a26ec45cffcc1bc41a92ab9af33076acbcb
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2023-10-05 16:43:08 +0000

    add placeholders and subpages, i hope it works

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 window.onload = () => {
-    document.getElementById('enl').classList.toggle("light")
+  document.getElementById('enl').classList.toggle("light")
+  document.body.classList.toggle('fadein')
+}
+
+function typeWriter(text, n, elem) {
+  if (n < (text.length)) {
+    $('.' + elem).html(text.substring(0, n + 1));
+    n++;
+    setTimeout(function() {
+      typeWriter(text, n, elem)
+    }, 100);
   }
-  
-  function typeWriter(text, n, elem) {
-    if (n < (text.length)) {
-      $('.' + elem).html(text.substring(0, n + 1));
-      n++;
-      setTimeout(function() {
-        typeWriter(text, n, elem)
-      }, 100);
-    }
-  }
-  
-  window.onload = (function(e) {
-    e.stopPropagation();
-  
-    var text = $('.test').data('text');
-    typeWriter(text, 0, 'test');
-    setTimeout(() => {
-      var text = $('.pssst').data('text');
-      typeWriter(text, 0, 'pssst');
-    }, 4000)
-    setTimeout(() => {
-      var text = $('.underlinedlinkth').data('text');
-      typeWriter(text, 0, 'underlinedlinkth');
-    }, 4500)
-    setTimeout(() => {
-      var text = $('.underlinedlinkjl').data('text');
-      typeWriter(text, 0, 'underlinedlinkjl');
-    }, 5000)
-  });
-  
+}
+
+window.onload = (function(e) {
+  e.stopPropagation();
+
+  var text = $('.test').data('text');
+  typeWriter(text, 0, 'test');
+  setTimeout(() => {
+    var text = $('.pssst').data('text');
+    typeWriter(text, 0, 'pssst');
+  }, 4000)
+  setTimeout(() => {
+    var text = $('.underlinedlinkth').data('text');
+    typeWriter(text, 0, 'underlinedlinkth');
+  }, 4500)
+  setTimeout(() => {
+    var text = $('.underlinedlinkjl').data('text');
+    typeWriter(text, 0, 'underlinedlinkjl');
+  }, 5000)
+});
+
+//document.onclick = () => {
+//	document.body.classList.toggle('fadeout')
+//}
+
